abeabced8a4b84a4037b0dc7b2d658534b012aa9
Marco Rotili <marco.rotili@handgemacht.ai>
2026-03-16 13:28:22 +0100

    ✨ feat: add PlanSource behaviour for plan data source abstraction

---
+++ lib/spotter/plans/plan_source.ex
@@ -0,0 +1,22 @@
+defmodule Spotter.Plans.PlanSource do
+  @moduledoc """
+  Behaviour for plan data sources.
+
+  Implementations provide access to project plans (epics) and their
+  child tasks from various backends (Dolt beads, local files, etc.).
+  """
+
+  alias Spotter.Beads.BeadStructs
+
+  @type plan :: BeadStructs.Epic.t()
+  @type task :: BeadStructs.Task.t()
+  @type project_summary :: %{project: String.t(), plan_count: integer()}
+
+  @callback list_projects() :: {:ok, [project_summary()]} | {:error, term()}
+  @callback list_plans(project :: String.t(), filter :: :all | :open | :closed) ::
+              {:ok, [plan()]} | {:error, term()}
+  @callback get_plan(project :: String.t(), plan_id :: String.t()) ::
+              {:ok, plan()} | {:error, term()}
+  @callback list_children(project :: String.t(), plan_id :: String.t()) ::
+              {:ok, [task()]} | {:error, term()}
+end
